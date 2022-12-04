41e3f791c2104126990eec07cff583278bde8b48
Kismetist <92977701+Kismetist@users.noreply.github.com>
2022-12-04 21:15:23 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,9 +21,9 @@ st.sidebar.info(
 st.title("可视化大作业")
 
 
-st.info("点击右侧来运行不同的展示项目")
+st.info("点击←侧来进行可视化展示")
 
-st.subheader("卫星延迟摄影")
+st.subheader("请欣赏")
 
 row1_col1, row1_col2 = st.columns(2)
 with row1_col1:
