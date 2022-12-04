86b8ae81aa39b3ed25b76def8ed1025da8d35b52
Kismetist <92977701+Kismetist@users.noreply.github.com>
2022-12-04 20:00:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,6 @@ st.set_page_config(layout="wide")
 st.title("Streamlit for Geospatial Applications")
 st.header("Instructions")
 
-
-st.markdown(markdown)
-
 m = leafmap.Map(minimap_control=True)
 m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=500)
