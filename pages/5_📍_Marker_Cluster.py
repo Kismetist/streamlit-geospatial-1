fbea82c0e6a8cc497dc7c80e3dfb5c48f0b446cb
Kismetist <92977701+Kismetist@users.noreply.github.com>
2022-12-04 21:12:33 +0800

    Update 5_📍_Marker_Cluster.py

--- pages/5_📍_Marker_Cluster.py
+++ pages/5_📍_Marker_Cluster.py
@@ -8,7 +8,8 @@ st.title("市场聚类图")
 
 with st.expander("See source code"):
     with st.echo():
-
+        cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
+        regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
         m = leafmap.Map(center=[40, -100], zoom=4)
         m.add_geojson(regions, layer_name='US Regions')
         m.add_points_from_xy(
