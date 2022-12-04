1054bbc2902831bce905e52dac6e30cadfe0eb55
Kismetist <92977701+Kismetist@users.noreply.github.com>
2022-12-04 20:41:40 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,51 +3,27 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-st.sidebar.title("About")
+st.sidebar.title("可视化")
 st.sidebar.info(
     """
-    Web App URL: <https://geospatial.streamlitapp.com>
-    GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+    Web App URL: <https://kismetist-streamlit-geospatial-1-home-076n41.streamlit.app/>
     """
 )
 
-st.sidebar.title("Contact")
+st.sidebar.title("小组成员")
 st.sidebar.info(
     """
-    Qiusheng Wu: <https://wetlands.io>
-    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
-    """
-)
-
-st.sidebar.title("Support")
-st.sidebar.info(
-    """
-    If you want to reward my work, I'd love a cup of coffee from you. Thanks!
-    [buymeacoffee.com/giswqs](http://buymeacoffee.com/giswqs)
+   赵瑞坤、方鹏贺、杨安恺、张泽宇
     """
 )
 
 
-st.title("Streamlit for Geospatial Applications")
+st.title("可视化大作业")
 
-st.markdown(
-    """
-    This multi-page web app demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and open-source mapping libraries, 
-    such as [leafmap](https://leafmap.org), [geemap](https://geemap.org), [pydeck](https://deckgl.readthedocs.io), and [kepler.gl](https://docs.kepler.gl/docs/keplergl-jupyter).
-    This is an open-source project and you are very welcome to contribute your comments, questions, resources, and apps as [issues](https://github.com/giswqs/streamlit-geospatial/issues) or 
-    [pull requests](https://github.com/giswqs/streamlit-geospatial/pulls) to the [GitHub repository](https://github.com/giswqs/streamlit-geospatial).
 
-    """
-)
+st.info("点击右侧来运行不同的展示项目")
 
-st.info("Click on the left sidebar menu to navigate to the different apps.")
-
-st.subheader("Timelapse of Satellite Imagery")
-st.markdown(
-    """
-    The following timelapse animations were created using the Timelapse web app. Click `Timelapse` on the left sidebar menu to create your own timelapse for any location around the globe.
-"""
-)
+st.subheader("卫星延迟摄影")
 
 row1_col1, row1_col2 = st.columns(2)
 with row1_col1:
